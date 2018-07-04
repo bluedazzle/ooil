dcc29770f81dca3f7710f60831004ed91535db41
zhangjianqi <zhangjianqi@bytedance.com>
2018-07-04 16:41:49 +0800

    cors
    Change-Id: I9fc33850ee042e120023328f48354229320b649a

--- OilP/settings.py
+++ OilP/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'core',
     'ckeditor',
     'ckeditor_uploader',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -50,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 )
 
 ROOT_URLCONF = 'OilP.urls'
